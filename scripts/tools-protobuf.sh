e74682cc783d91f35bd283c4fbd13001b90171bf
rajatlko13 <rajatlko13@gmail.com>
2021-11-02 20:28:17 +0530

    Finalized the script

--- scripts/tools-protobuf.sh
+++ scripts/tools-protobuf.sh
@@ -1,7 +1,5 @@
 curl -LO https://github.com/protocolbuffers/protobuf/releases/download/v3.12.0/protoc-3.12.0-linux-x86_64.zip
-unzip protoc-3.12.0-linux-x86_64.zip -d $HOME/.local
-export PATH="$PATH:$HOME/.local/bin"
+sudo unzip protoc-3.12.0-linux-x86_64.zip -d /usr/local/bin
 
-go get google.golang.org/grpc
-
-sudo apt-get install -y golang-protobuf-extensions-dev
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.25.0
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1
