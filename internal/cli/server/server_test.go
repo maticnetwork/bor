a22de72d90fb5ccd47f166db5e94eba477646bb7
Manav Darji <manavdarji.india@gmail.com>
2022-02-02 21:03:29 +0530

    refactor cli tests

--- internal/cli/server/server_test.go
+++ internal/cli/server/server_test.go
@@ -1,14 +1,34 @@
 package server
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"strconv"
+	"sync/atomic"
 	"testing"
 	"time"
 
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestServer_DeveloperMode(t *testing.T) {
+var initialPort uint64 = 61000
 
+// nextPort gives the next available port starting from 60000
+func nextPort() uint64 {
+	log.Info("Checking for new port", "current", initialPort)
+	port := atomic.AddUint64(&initialPort, 1)
+	addr := fmt.Sprintf("localhost:%d", port)
+	if _, err := net.Listen("tcp", addr); err == nil {
+		return port
+	} else {
+		return nextPort()
+	}
+}
+
+func TestServer_DeveloperMode(t *testing.T) {
 	// get the default config
 	config := DefaultConfig()
 
@@ -16,17 +36,25 @@ func TestServer_DeveloperMode(t *testing.T) {
 	config.Developer.Enabled = true
 	config.Developer.Period = 2 // block time
 
+	// grpc port
+	port := strconv.Itoa(int(nextPort()))
+	config.GRPC.Addr = ":" + port
+
+	// datadir
+	datadir, _ := ioutil.TempDir("/tmp", "bor-cli-test")
+	config.DataDir = datadir
+	defer os.RemoveAll(datadir)
+
 	// start the server
-	server, err1 := NewServer(config)
-	if err1 != nil {
-		t.Fatalf("failed to start server: %v", err1)
-	}
+	server, err := NewServer(config)
+	assert.NoError(t, err)
+	defer server.Stop()
 
 	// record the initial block number
 	blockNumber := server.backend.BlockChain().CurrentBlock().Header().Number.Int64()
 
 	var i int64 = 0
-	for i = 0; i < 10; i++ {
+	for i = 0; i < 3; i++ {
 		// We expect the node to mine blocks every `config.Developer.Period` time period
 		time.Sleep(time.Duration(config.Developer.Period) * time.Second)
 		currBlock := server.backend.BlockChain().CurrentBlock().Header().Number.Int64()
@@ -35,7 +63,4 @@ func TestServer_DeveloperMode(t *testing.T) {
 			break
 		}
 	}
-
-	// stop the server
-	server.Stop()
 }
