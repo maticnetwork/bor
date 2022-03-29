e78260fac9eae696b8cfaf8d1570b55dc14d15c1
Wany <q873040807@gmail.com>
2022-03-29 15:44:48 +0800

    fix GetTransactionReceiptsByBlock bug
    fix fields transactionHash

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -669,6 +669,7 @@ func (s *PublicBlockChainAPI) GetTransactionReceiptsByBlock(ctx context.Context,
 			"contractAddress":   nil,
 			"logs":              receipt.Logs,
 			"logsBloom":         receipt.Bloom,
+			"type":              hexutil.Uint(tx.Type()),
 		}
 
 		// Assign receipt status or post state.
@@ -680,7 +681,7 @@ func (s *PublicBlockChainAPI) GetTransactionReceiptsByBlock(ctx context.Context,
 		if receipt.Logs == nil {
 			fields["logs"] = [][]*types.Log{}
 		}
-		if borReceipt != nil {
+		if borReceipt != nil && idx == len(receipts)-1 {
 			fields["transactionHash"] = txHash
 		}
 		// If the ContractAddress is 20 0x0 bytes, assume it is not a contract creation
