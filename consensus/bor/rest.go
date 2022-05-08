13d0ba25e30c67e198af6a0d9463fe6e53a13bbe
Manav Darji <manavdarji.india@gmail.com>
2022-05-08 12:07:20 +0530

    fix: rm comment

--- consensus/bor/rest.go
+++ consensus/bor/rest.go
@@ -91,7 +91,6 @@ func (h *HeimdallClient) FetchStateSyncEvents(fromID uint64, to int64) ([]*Event
 
 // FetchLatestCheckpoint fetches the latest bor submitted checkpoint from heimdall
 func (h *HeimdallClient) FetchLatestCheckpoint() (*Checkpoint, error) {
-	// var checkpoint Checkpoint
 	checkpoint := &Checkpoint{}
 
 	response, err := h.Fetch("/checkpoints/latest", "")
