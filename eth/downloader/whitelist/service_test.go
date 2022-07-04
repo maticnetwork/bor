c34fcba7c88e1961f0b0b176bdd26d989e227214
Evgeny Danienko <6655321@bk.ru>
2022-07-04 15:06:53 +0300

    split chain property tests

--- eth/downloader/whitelist/service_test.go
+++ eth/downloader/whitelist/service_test.go
@@ -2,14 +2,17 @@ package whitelist
 
 import (
 	"errors"
+	"fmt"
 	"math/big"
 	"reflect"
+	"sort"
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"gotest.tools/assert"
 )
 
 // NewMockService creates a new mock whitelist service
@@ -30,11 +33,11 @@ func TestWhitelistCheckpoint(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		s.enqueueCheckpointWhitelist(uint64(i), common.Hash{})
 	}
-	assert.Equal(t, s.length(), 10, "expected 10 items in whitelist")
+	require.Equal(t, s.length(), 10, "expected 10 items in whitelist")
 
 	s.enqueueCheckpointWhitelist(11, common.Hash{})
 	s.dequeueCheckpointWhitelist()
-	assert.Equal(t, s.length(), 10, "expected 10 items in whitelist")
+	require.Equal(t, s.length(), 10, "expected 10 items in whitelist")
 }
 
 // TestIsValidPeer checks the IsValidPeer function in isolation
@@ -46,14 +49,14 @@ func TestIsValidPeer(t *testing.T) {
 
 	// case1: no checkpoint whitelist, should consider the chain as valid
 	res, err := s.IsValidPeer(nil, nil)
-	assert.NilError(t, err, "expected no error")
-	assert.Equal(t, res, true, "expected chain to be valid")
+	require.NoError(t, err, "expected no error")
+	require.Equal(t, res, true, "expected chain to be valid")
 
 	// add checkpoint entries and mock fetchHeadersByNumber function
 	s.ProcessCheckpoint(uint64(0), common.Hash{})
 	s.ProcessCheckpoint(uint64(1), common.Hash{})
 
-	assert.Equal(t, s.length(), 2, "expected 2 items in whitelist")
+	require.Equal(t, s.length(), 2, "expected 2 items in whitelist")
 
 	// create a false function, returning absolutely nothing
 	falseFetchHeadersByNumber := func(number uint64, amount int, skip int, reverse bool) ([]*types.Header, []common.Hash, error) {
@@ -71,7 +74,7 @@ func TestIsValidPeer(t *testing.T) {
 		t.Fatalf("expected error ErrNoRemoteCheckoint, got %v", err)
 	}
 
-	assert.Equal(t, res, false, "expected chain to be invalid")
+	require.Equal(t, res, false, "expected chain to be invalid")
 
 	// case3: correct fetchHeadersByNumber function provided, should consider the chain as valid
 	// create a mock function, returning a the required header
@@ -94,18 +97,18 @@ func TestIsValidPeer(t *testing.T) {
 	}
 
 	res, err = s.IsValidPeer(nil, fetchHeadersByNumber)
-	assert.NilError(t, err, "expected no error")
-	assert.Equal(t, res, true, "expected chain to be valid")
+	require.NoError(t, err, "expected no error")
+	require.Equal(t, res, true, "expected chain to be valid")
 
 	// add one more checkpoint whitelist entry
 	s.ProcessCheckpoint(uint64(2), common.Hash{})
-	assert.Equal(t, s.length(), 3, "expected 3 items in whitelist")
+	require.Equal(t, s.length(), 3, "expected 3 items in whitelist")
 
 	// case4: correct fetchHeadersByNumber function provided with wrong header
 	// for block number 2. Should consider the chain as invalid and throw an error
 	res, err = s.IsValidPeer(nil, fetchHeadersByNumber)
-	assert.Equal(t, err, ErrCheckpointMismatch, "expected checkpoint mismatch error")
-	assert.Equal(t, res, false, "expected chain to be invalid")
+	require.Equal(t, err, ErrCheckpointMismatch, "expected checkpoint mismatch error")
+	require.Equal(t, res, false, "expected chain to be invalid")
 }
 
 // TestIsValidChain checks the IsValidChain function in isolation
@@ -117,7 +120,7 @@ func TestIsValidChain(t *testing.T) {
 	chainA := createMockChain(1, 20) // A1->A2...A19->A20
 	// case1: no checkpoint whitelist, should consider the chain as valid
 	res := s.IsValidChain(nil, chainA)
-	assert.Equal(t, res, true, "expected chain to be valid")
+	require.Equal(t, res, true, "expected chain to be valid")
 
 	tempChain := createMockChain(21, 22) // A21->A22
 
@@ -125,49 +128,49 @@ func TestIsValidChain(t *testing.T) {
 	s.ProcessCheckpoint(tempChain[0].Number.Uint64(), tempChain[0].Hash())
 	s.ProcessCheckpoint(tempChain[1].Number.Uint64(), tempChain[1].Hash())
 
-	assert.Equal(t, s.length(), 2, "expected 2 items in whitelist")
+	require.Equal(t, s.length(), 2, "expected 2 items in whitelist")
 
 	// case2: We're behind the oldest whitelisted block entry, should consider
 	// the chain as valid as we're still far behind the latest blocks
 	res = s.IsValidChain(chainA[len(chainA)-1], chainA)
-	assert.Equal(t, res, true, "expected chain to be valid")
+	require.Equal(t, res, true, "expected chain to be valid")
 
 	// Clear checkpoint whitelist and add blocks A5 and A15 in whitelist
 	s.PurgeCheckpointWhitelist()
 	s.ProcessCheckpoint(chainA[5].Number.Uint64(), chainA[5].Hash())
 	s.ProcessCheckpoint(chainA[15].Number.Uint64(), chainA[15].Hash())
 
-	assert.Equal(t, s.length(), 2, "expected 2 items in whitelist")
+	require.Equal(t, s.length(), 2, "expected 2 items in whitelist")
 
 	// case3: Try importing a past chain having valid checkpoint, should
 	// consider the chain as valid
 	res = s.IsValidChain(chainA[len(chainA)-1], chainA)
-	assert.Equal(t, res, true, "expected chain to be valid")
+	require.Equal(t, res, true, "expected chain to be valid")
 
 	// Clear checkpoint whitelist and mock blocks in whitelist
 	tempChain = createMockChain(20, 20) // A20
 	s.PurgeCheckpointWhitelist()
 	s.ProcessCheckpoint(tempChain[0].Number.Uint64(), tempChain[0].Hash())
 
-	assert.Equal(t, s.length(), 1, "expected 1 items in whitelist")
+	require.Equal(t, s.length(), 1, "expected 1 items in whitelist")
 
 	// case4: Try importing a past chain having invalid checkpoint
 	res = s.IsValidChain(chainA[len(chainA)-1], chainA)
-	assert.Equal(t, res, false, "expected chain to be invalid")
+	require.Equal(t, res, false, "expected chain to be invalid")
 
 	// create a future chain to be imported of length <= `checkpointInterval`
 	chainB := createMockChain(21, 30) // B21->B22...B29->B30
 
 	// case5: Try importing a future chain of acceptable length
 	res = s.IsValidChain(chainA[len(chainA)-1], chainB)
-	assert.Equal(t, res, true, "expected chain to be valid")
+	require.Equal(t, res, true, "expected chain to be valid")
 
 	// create a future chain to be imported of length > `checkpointInterval`
 	chainB = createMockChain(21, 40) // C21->C22...C39->C40
 
 	// case5: Try importing a future chain of unacceptable length
 	res = s.IsValidChain(chainA[len(chainA)-1], chainB)
-	assert.Equal(t, res, false, "expected chain to be invalid")
+	require.Equal(t, res, false, "expected chain to be invalid")
 }
 
 func TestSplitChain(t *testing.T) {
@@ -202,17 +205,19 @@ func TestSplitChain(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 			past, future := splitChain(tc.current, tc.chain)
-			assert.Equal(t, len(past), tc.result.pastLength)
-			assert.Equal(t, len(future), tc.result.futureLength)
+			require.Equal(t, len(past), tc.result.pastLength)
+			require.Equal(t, len(future), tc.result.futureLength)
+
 			if len(past) > 0 {
 				// Check if we have expected block/s
-				assert.Equal(t, past[0].Number.Uint64(), tc.result.pastStart)
-				assert.Equal(t, past[len(past)-1].Number.Uint64(), tc.result.pastEnd)
+				require.Equal(t, past[0].Number.Uint64(), tc.result.pastStart)
+				require.Equal(t, past[len(past)-1].Number.Uint64(), tc.result.pastEnd)
 			}
+
 			if len(future) > 0 {
 				// Check if we have expected block/s
-				assert.Equal(t, future[0].Number.Uint64(), tc.result.futureStart)
-				assert.Equal(t, future[len(future)-1].Number.Uint64(), tc.result.futureEnd)
+				require.Equal(t, future[0].Number.Uint64(), tc.result.futureStart)
+				require.Equal(t, future[len(future)-1].Number.Uint64(), tc.result.futureEnd)
 			}
 		})
 	}
@@ -223,50 +228,131 @@ func TestSplitChainProperties(t *testing.T) {
 
 	// Current chain is at block: X
 	// Incoming chain is represented as [N, M]
-	testCases := []struct {
-		name    string
-		current uint64
-		chain   []*types.Header
-	}{
-		{name: "X = 10, N = 11, M = 20", current: uint64(10), chain: createMockChain(11, 20)},
-		{name: "X = 10, N = 13, M = 20", current: uint64(10), chain: createMockChain(13, 20)},
-		{name: "X = 10, N = 2, M = 10", current: uint64(10), chain: createMockChain(2, 10)},
-		{name: "X = 10, N = 2, M = 9", current: uint64(10), chain: createMockChain(2, 9)},
-		{name: "X = 10, N = 2, M = 8", current: uint64(10), chain: createMockChain(2, 8)},
-		{name: "X = 10, N = 5, M = 15", current: uint64(10), chain: createMockChain(5, 15)},
-		{name: "X = 10, N = 10, M = 20", current: uint64(10), chain: createMockChain(10, 20)},
+
+	currentChain := []int{0, 1, 2, 3, 10, 100} // blocks starting from genesis
+
+	type testCase struct {
+		current     int
+		remoteStart int
+		remoteEnd   int
 	}
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
+
+	var ts []testCase
+
+	blockDiffs := []int{0, 1, 2, 3, 4, 5, 9, 10, 11, 12, 90, 100, 101, 102}
+
+	for _, current := range currentChain {
+		// past cases only + past to current
+		for _, diff := range blockDiffs {
+			from := current - diff
+
+			// use int type for everything to not care about underflow
+			if from < 0 {
+				continue
+			}
+
+			for _, diff := range blockDiffs {
+				to := current - diff
+
+				if to >= from {
+					ts = append(ts, testCase{current, from, to})
+				}
+			}
+		}
+
+		// future only + current to future
+		for _, diff := range blockDiffs {
+			from := current + diff
+
+			if from < 0 {
+				continue
+			}
+
+			for _, diff := range blockDiffs {
+				to := current + diff
+
+				if to >= from {
+					ts = append(ts, testCase{current, from, to})
+				}
+			}
+		}
+
+		// past-current-future
+		// we don't care about duplicates
+		for _, diff := range blockDiffs {
+			from := current - diff
+
+			if from < 0 {
+				continue
+			}
+
+			for _, diff := range blockDiffs {
+				to := current + diff
+
+				if to >= from {
+					ts = append(ts, testCase{current, from, to})
+				}
+			}
+		}
+	}
+
+	for i, tc := range ts {
+		tc := tc
+
+		name := fmt.Sprintf("test case: index = %d, X = %d, N = %d, M = %d", i, tc.current, tc.remoteStart, tc.remoteEnd)
+
+		t.Run(name, func(t *testing.T) {
 			t.Parallel()
-			past, future := splitChain(tc.current, tc.chain)
+
+			chain := createMockChain(uint64(tc.remoteStart), uint64(tc.remoteEnd))
+
+			past, future := splitChain(uint64(tc.current), chain)
+
+			// properties
 			if len(past) > 0 {
 				// Check if the chain is ordered
-				for i := 1; i < len(past); i++ {
-					assert.Equal(t, past[i].Number.Uint64(), past[i-1].Number.Uint64()+1)
-				}
+				isOrdered := sort.SliceIsSorted(past, func(i, j int) bool {
+					return past[i].Number.Uint64() < past[j].Number.Uint64()
+				})
+
+				require.True(t, isOrdered, "an ordered past chain expected: %v", past)
+
+				isSequential := sort.SliceIsSorted(past, func(i, j int) bool {
+					return past[i].Number.Uint64() == past[j].Number.Uint64()-1
+				})
+
+				require.True(t, isSequential, "a sequential past chain expected: %v", past)
 
 				// Check if current block >= past chain's last block
-				assert.Equal(t, past[len(past)-1].Number.Uint64() <= tc.current, true)
+				require.Equal(t, past[len(past)-1].Number.Uint64() <= uint64(tc.current), true)
 			}
+
 			if len(future) > 0 {
 				// Check if the chain is ordered
-				for i := 1; i < len(future); i++ {
-					assert.Equal(t, future[i].Number.Uint64(), future[i-1].Number.Uint64()+1)
-				}
+				isOrdered := sort.SliceIsSorted(future, func(i, j int) bool {
+					return future[i].Number.Uint64() < future[j].Number.Uint64()
+				})
+
+				require.True(t, isOrdered, "an ordered future chain expected: %v", future)
+
+				isSequential := sort.SliceIsSorted(future, func(i, j int) bool {
+					return future[i].Number.Uint64() == future[j].Number.Uint64()-1
+				})
+
+				require.True(t, isSequential, "a sequential future chain expected: %v", future)
 
 				// Check if future chain's first block > current block
-				assert.Equal(t, future[len(future)-1].Number.Uint64() > tc.current, true)
+				require.Equal(t, future[len(future)-1].Number.Uint64() > uint64(tc.current), true)
 			}
 
 			// Check if both chains are continuous
 			if len(past) > 0 && len(future) > 0 {
-				assert.Equal(t, past[len(past)-1].Number.Uint64(), future[0].Number.Uint64()-1)
+				require.Equal(t, past[len(past)-1].Number.Uint64(), future[0].Number.Uint64()-1)
 			}
 
 			// Check if we get the original chain on appending both
-			chain := append(past, future...)
-			assert.Equal(t, reflect.DeepEqual(chain, tc.chain), true)
+			gotChain := append(past, future...)
+			require.Equal(t, reflect.DeepEqual(gotChain, chain), true)
 		})
 	}
 }
