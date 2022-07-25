9a428f99bc3b463d256f70de494797384daec4de
Manav Darji <manavdarji.india@gmail.com>
2022-07-25 16:15:15 +0530

    create checkpoint verifier, add unit tests

--- eth/handler_bor.go
+++ eth/handler_bor.go
@@ -3,13 +3,10 @@ package eth
 import (
 	"context"
 	"errors"
-	"fmt"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/consensus/bor"
 	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/rpc"
 )
 
 var (
@@ -43,7 +40,7 @@ var (
 
 // fetchWhitelistCheckpoints fetches the latest checkpoint/s from it's local heimdall
 // and verifies the data against bor data.
-func (h *ethHandler) fetchWhitelistCheckpoints(ctx context.Context, bor *bor.Bor, first bool) ([]uint64, []common.Hash, error) {
+func (h *ethHandler) fetchWhitelistCheckpoints(ctx context.Context, bor *bor.Bor, checkpointVerifier *checkpointVerifier, first bool) ([]uint64, []common.Hash, error) {
 	// Create an array for block number and block hashes
 	//nolint:prealloc
 	var (
@@ -89,33 +86,14 @@ func (h *ethHandler) fetchWhitelistCheckpoints(ctx context.Context, bor *bor.Bor
 			return blockNums, blockHashes, errCheckpoint
 		}
 
-		// check if we have the checkpoint blocks
-		head := h.ethAPI.BlockNumber()
-		if head < hexutil.Uint64(checkpoint.EndBlock.Uint64()) {
-			log.Debug("Head block behind checkpoint block", "head", head, "checkpoint end block", checkpoint.EndBlock)
-			return blockNums, blockHashes, errMissingCheckpoint
-		}
+		// Verify if the checkpoint fetched can be added to the local whitelist entry or not
+		// If verified, it returns the hash of the end block of the checkpoint. If not,
+		// it will return appropriate error.
 
-		// verify the root hash of checkpoint
-		roothash, err := h.ethAPI.GetRootHash(ctx, checkpoint.StartBlock.Uint64(), checkpoint.EndBlock.Uint64())
+		hash, err := checkpointVerifier.verify(ctx, h, checkpoint)
 		if err != nil {
-			log.Debug("Failed to get root hash of checkpoint while whitelisting", "err", err)
-			return blockNums, blockHashes, errRootHash
+			return blockNums, blockHashes, err
 		}
-
-		if roothash != checkpoint.RootHash.String()[2:] {
-			log.Warn("Checkpoint root hash mismatch while whitelisting", "expected", checkpoint.RootHash.String()[2:], "got", roothash)
-			return blockNums, blockHashes, errCheckpointRootHashMismatch
-		}
-
-		// fetch the end checkpoint block hash
-		block, err := h.ethAPI.GetBlockByNumber(ctx, rpc.BlockNumber(checkpoint.EndBlock.Uint64()), false)
-		if err != nil {
-			log.Debug("Failed to get end block hash of checkpoint while whitelisting", "err", err)
-			return blockNums, blockHashes, errEndBlock
-		}
-
-		hash := fmt.Sprintf("%v", block["hash"])
 
 		blockNums = append(blockNums, checkpoint.EndBlock.Uint64())
 		blockHashes = append(blockHashes, common.HexToHash(hash))
