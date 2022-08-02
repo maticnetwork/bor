a99ff6e7d1e984b810125a48dc186a37055827be
Manav Darji <manavdarji.india@gmail.com>
2022-08-02 22:27:52 +0530

    address comment for default checkpoint verifier

--- eth/bor_checkpoint_verifier.go
+++ eth/bor_checkpoint_verifier.go
@@ -15,38 +15,45 @@ type checkpointVerifier struct {
 }
 
 func newCheckpointVerifier(verifyFn func(ctx context.Context, handler *ethHandler, checkpoint *checkpoint.Checkpoint) (string, error)) *checkpointVerifier {
-	if verifyFn == nil {
-		verifyFn = func(ctx context.Context, handler *ethHandler, checkpoint *checkpoint.Checkpoint) (string, error) {
-			// check if we have the checkpoint blocks
-			head := handler.ethAPI.BlockNumber()
-			if head < hexutil.Uint64(checkpoint.EndBlock.Uint64()) {
-				log.Debug("Head block behind checkpoint block", "head", head, "checkpoint end block", checkpoint.EndBlock)
-				return "", errMissingCheckpoint
-			}
+	if verifyFn != nil {
+		return &checkpointVerifier{verifyFn}
+	}
 
-			// verify the root hash of checkpoint
-			roothash, err := handler.ethAPI.GetRootHash(ctx, checkpoint.StartBlock.Uint64(), checkpoint.EndBlock.Uint64())
-			if err != nil {
-				log.Debug("Failed to get root hash of checkpoint while whitelisting", "err", err)
-				return "", errRootHash
-			}
+	verifyFn = func(ctx context.Context, handler *ethHandler, checkpoint *checkpoint.Checkpoint) (string, error) {
+		var (
+			startBlock = checkpoint.StartBlock.Uint64()
+			endBlock   = checkpoint.EndBlock.Uint64()
+		)
 
-			if roothash != checkpoint.RootHash.String()[2:] {
-				log.Warn("Checkpoint root hash mismatch while whitelisting", "expected", checkpoint.RootHash.String()[2:], "got", roothash)
-				return "", errCheckpointRootHashMismatch
-			}
-
-			// fetch the end checkpoint block hash
-			block, err := handler.ethAPI.GetBlockByNumber(ctx, rpc.BlockNumber(checkpoint.EndBlock.Uint64()), false)
-			if err != nil {
-				log.Debug("Failed to get end block hash of checkpoint while whitelisting", "err", err)
-				return "", errEndBlock
-			}
-
-			hash := fmt.Sprintf("%v", block["hash"])
-
-			return hash, nil
+		// check if we have the checkpoint blocks
+		head := handler.ethAPI.BlockNumber()
+		if head < hexutil.Uint64(endBlock) {
+			log.Debug("Head block behind checkpoint block", "head", head, "checkpoint end block", endBlock)
+			return "", errMissingCheckpoint
 		}
+
+		// verify the root hash of checkpoint
+		roothash, err := handler.ethAPI.GetRootHash(ctx, startBlock, endBlock)
+		if err != nil {
+			log.Debug("Failed to get root hash of checkpoint while whitelisting", "err", err)
+			return "", errRootHash
+		}
+
+		if roothash != checkpoint.RootHash.String()[2:] {
+			log.Warn("Checkpoint root hash mismatch while whitelisting", "expected", checkpoint.RootHash.String()[2:], "got", roothash)
+			return "", errCheckpointRootHashMismatch
+		}
+
+		// fetch the end checkpoint block hash
+		block, err := handler.ethAPI.GetBlockByNumber(ctx, rpc.BlockNumber(endBlock), false)
+		if err != nil {
+			log.Debug("Failed to get end block hash of checkpoint while whitelisting", "err", err)
+			return "", errEndBlock
+		}
+
+		hash := fmt.Sprintf("%v", block["hash"])
+
+		return hash, nil
 	}
 
 	return &checkpointVerifier{verifyFn}
