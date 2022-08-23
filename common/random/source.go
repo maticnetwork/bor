1807d3ae595d7c9f0b8d546b158521aeec61b328
Evgeny Danienko <6655321@bk.ru>
2022-08-23 13:24:52 +0300

    fmt

--- common/random/source.go
+++ common/random/source.go
@@ -13,7 +13,7 @@ func (s CryptoSource) Int63() int64 {
 	return int64(s.Uint64() & ^uint64(1<<63))
 }
 
-func (s CryptoSource) Uint64() (v uint64) {
+func (CryptoSource) Uint64() (v uint64) {
 	_ = binary.Read(rand.Reader, binary.BigEndian, &v)
 
 	return v
