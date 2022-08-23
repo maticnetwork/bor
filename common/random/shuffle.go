5771a1ac8b97fb15b10af98b7fd3d5f3836155a9
Evgeny Danienko <6655321@bk.ru>
2022-08-23 13:21:28 +0300

    linters

--- common/random/shuffle.go
+++ common/random/shuffle.go
@@ -5,7 +5,8 @@ import (
 )
 
 func Shuffle[T any](slice []T) {
-	r := rand.New(CryptoSource{})
+	//nolint:gosec
+	r := rand.New(CryptoSource{}) //CryptoSource is a cryptographic source of randomness
 
 	r.Shuffle(len(slice), func(i, j int) {
 		slice[i], slice[j] = slice[j], slice[i]
@@ -17,6 +18,7 @@ func ShuffleMap[K comparable, V any](m map[K]V) []K {
 	keys := make([]K, len(m))
 
 	i := 0
+
 	for key := range m {
 		keys[i] = key
 		i++
