02bc70406bc04664e86891f4f4307363f5438f88
Jerry <jerrycgh@gmail.com>
2022-10-18 12:18:01 -0700

    Remove data from logging

--- miner/worker.go
+++ miner/worker.go
@@ -965,7 +965,7 @@ func (w *worker) commitTransactions(env *environment, txs *types.TransactionsByP
 		// Start executing the transaction
 		env.state.Prepare(tx.Hash(), env.tcount)
 
-		log.Info("Commit new tx", "tx hash", tx.Hash(), "from", from, "to", tx.To(), "nonce", tx.Nonce(), "gas", tx.Gas(), "gasPrice", tx.GasPrice(), "value", tx.Value(), "data", tx.Data())
+		log.Info("Commit new tx", "tx hash", tx.Hash(), "from", from, "to", tx.To(), "nonce", tx.Nonce(), "gas", tx.Gas(), "gasPrice", tx.GasPrice(), "value", tx.Value())
 
 		start := time.Now()
 
