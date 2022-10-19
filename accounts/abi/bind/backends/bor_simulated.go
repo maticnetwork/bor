9a3e1f6b43a45cb9cf7e5fbc7301d57152ebc27e
Vaibhav Jindal <vaibhavjindal29@gmail.com>
2022-10-19 16:33:24 +0530

    Added the GetVoteOnRootHash in filterBackend

--- accounts/abi/bind/backends/bor_simulated.go
+++ accounts/abi/bind/backends/bor_simulated.go
@@ -23,6 +23,10 @@ func (fb *filterBackend) GetBorBlockReceipt(ctx context.Context, hash common.Has
 	return receipt, nil
 }
 
+func (fb *filterBackend) GetVoteOnRootHash(ctx context.Context, starBlockNr uint64, endBlockNr uint64, rootHash string, milestoneId string) (bool, error) {
+	return false, nil
+}
+
 func (fb *filterBackend) GetBorBlockLogs(ctx context.Context, hash common.Hash) ([]*types.Log, error) {
 	receipt, err := fb.GetBorBlockReceipt(ctx, hash)
 	if err != nil || receipt == nil {
