35d0ed751b7db7e09d1119bea94f8ecf11cb37c1
Krishna Upadhyaya <krishnau1604@gmail.com>
2022-12-20 19:15:17 +0530

    Remove unwanted changes

--- accounts/abi/bind/bind_test.go
+++ accounts/abi/bind/bind_test.go
@@ -2020,18 +2020,9 @@ func TestGolangBindings(t *testing.T) {
 	pwd, _ := os.Getwd()
 	replacer := exec.Command(gocmd, "mod", "edit", "-x", "-require", "github.com/ethereum/go-ethereum@v0.0.0", "-replace", "github.com/ethereum/go-ethereum="+filepath.Join(pwd, "..", "..", "..")) // Repo root
 	replacer.Dir = pkg
-
 	if out, err := replacer.CombinedOutput(); err != nil {
 		t.Fatalf("failed to replace binding test dependency to current source tree: %v\n%s", err, out)
 	}
-
-	replacer = exec.Command(gocmd, "mod", "edit", "-x", "-replace", "github.com/btcsuite/btcd/chaincfg/chainhash="+"github.com/btcsuite/btcd/chaincfg/chainhash@v1.0.0")
-	replacer.Dir = pkg
-
-	if out, err := replacer.CombinedOutput(); err != nil {
-		t.Fatalf("failed to replace binding test dependency to current source tree: %v\n%s", err, out)
-	}
-
 	tidier := exec.Command(gocmd, "mod", "tidy")
 	tidier.Dir = pkg
 	if out, err := tidier.CombinedOutput(); err != nil {
