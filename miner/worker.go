921e428532e24ffd8d19a4374e88b17d5da940c7
Shivam Sharma <shivam691999@gmail.com>
2023-01-16 19:08:20 +0530

    lint : fix linters

--- miner/worker.go
+++ miner/worker.go
@@ -1042,6 +1042,7 @@ func (w *worker) commitTransactions(env *environment, txs *types.TransactionsByP
 			coalescedLogs = append(coalescedLogs, logs...)
 			env.tcount++
 			txs.Shift()
+
 			if log.Root().GetHandler().Level() >= log.LvlDebug {
 				log.Debug("Committed new tx", "tx hash", tx.Hash(), "from", from, "to", tx.To(), "nonce", tx.Nonce(), "gas", tx.Gas(), "gasPrice", tx.GasPrice(), "value", tx.Value(), "time spent", time.Since(start))
 			}
