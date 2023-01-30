9d8ad447556dc69a6fd7f9bfe2e1837825485db1
Pratik Patil <pratikspatil024@gmail.com>
2023-01-30 12:49:36 +0530

    checking if backend is available during status call, and added a flag to wait if backend is not available

--- internal/cli/debug_pprof.go
+++ internal/cli/debug_pprof.go
@@ -7,8 +7,6 @@ import (
 	"fmt"
 	"strings"
 
-	empty "google.golang.org/protobuf/types/known/emptypb"
-
 	"github.com/ethereum/go-ethereum/internal/cli/flagset"
 	"github.com/ethereum/go-ethereum/internal/cli/server/proto"
 )
@@ -133,7 +131,7 @@ func (d *DebugPprofCommand) Run(args []string) int {
 
 	// append the status
 	{
-		statusResp, err := clt.Status(ctx, &empty.Empty{})
+		statusResp, err := clt.Status(ctx, &proto.StatusRequest{})
 		if err != nil {
 			d.UI.Output(fmt.Sprintf("Failed to get status: %v", err))
 			return 1
