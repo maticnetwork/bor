6b1314cf56a3add0cfedab7b8b0ed2729cd52aa2
Evgeny Danienko <6655321@bk.ru>
2023-02-02 12:23:19 +0400

    simplify things

--- rpc/execution_pool.go
+++ rpc/execution_pool.go
@@ -1,6 +1,7 @@
 package rpc
 
 import (
+	"context"
 	"sync/atomic"
 	"time"
 
@@ -9,18 +10,45 @@ import (
 
 const (
 	threads        = 100
-	requestTimeout = 10 * time.Second
+	requestTimeout = 0 //10 * time.Second
 )
 
-//nolint:unused
-func changePoolSize(execPool *atomic.Pointer[workerpool.WorkerPool], n int) {
-	oldPool := execPool.Load()
+type SafePool struct {
+	executionPool *atomic.Pointer[workerpool.WorkerPool]
+}
+
+func NewExecutionPool(initialSize int) *SafePool {
+	if initialSize == 0 {
+		return &SafePool{}
+	}
+
+	var ptr atomic.Pointer[workerpool.WorkerPool]
+
+	p := workerpool.New(initialSize)
+	ptr.Store(p)
+
+	return &SafePool{&ptr}
+}
+
+func (s *SafePool) Submit(ctx context.Context, fn func() error, timeout ...time.Duration) (<-chan error, bool) {
+	pool := s.executionPool.Load()
+	if pool != nil {
+		return pool.Submit(ctx, fn, timeout...), true
+	}
+
+	go func() {
+		_ = fn()
+	}()
+
+	return nil, false
+}
+
+func (s *SafePool) ChangeSize(n int) {
+	oldPool := s.executionPool.Swap(workerpool.New(n))
 
 	if oldPool != nil {
 		go func() {
 			oldPool.StopWait()
 		}()
 	}
-
-	execPool.Store(workerpool.New(n))
 }
