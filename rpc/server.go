2476c082e103f2c91ce7b2cf1245d5c950834205
Pratik Patil <pratikspatil024@gmail.com>
2023-03-09 10:21:25 +0530

    more weird lints from develop

--- rpc/server.go
+++ rpc/server.go
@@ -105,10 +105,13 @@ func (s *Server) serveSingleRequest(ctx context.Context, codec ServerCodec) {
 	reqs, batch, err := codec.readBatch()
 	if err != nil {
 		if err != io.EOF {
+			// nolint: errcheck
 			codec.writeJSON(ctx, errorMessage(&invalidMessageError{"parse error"}))
 		}
 		return
 	}
+
+	// nolint: contextcheck
 	if batch {
 		h.handleBatch(reqs)
 	} else {
