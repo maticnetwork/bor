4694e63ead9d48b280e2cfd5bedc41944da40b07
Pratik Patil <pratikspatil024@gmail.com>
2023-04-10 11:29:59 +0530

    removed some logs

--- consensus/bor/bor.go
+++ consensus/bor/bor.go
@@ -754,12 +754,6 @@ func (c *Bor) Finalize(chain consensus.ChainHeaderReader, header *types.Header, 
 		err           error
 	)
 
-	if c.chainConfig.Bor.IsIndore(header.Number) {
-		fmt.Println("PSP - in Finalize - post Indore. current block", header.Number, "Indore block", c.chainConfig.Bor.IndoreBlock)
-	} else {
-		fmt.Println("PSP - in Finalize - pre Indore", header.Number, "Indore block", c.chainConfig.Bor.IndoreBlock)
-	}
-
 	headerNumber := header.Number.Uint64()
 
 	if IsSprintStart(headerNumber, c.config.CalculateSprint(headerNumber)) {
