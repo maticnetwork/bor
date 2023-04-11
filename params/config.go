a2212769907babc0259067827feccf5156d99000
Pratik Patil <pratikspatil024@gmail.com>
2023-04-11 17:04:40 +0530

    Revert "added few logs"
    This reverts commit b1509ff736f0fa9e84711a2367f762b1cfc55647.

--- params/config.go
+++ params/config.go
@@ -609,7 +609,6 @@ func (c *BorConfig) CalculateSprint(number uint64) uint64 {
 }
 
 func (c *BorConfig) FetchStateSyncMultiplier(number uint64) uint64 {
-	fmt.Println("c.StateSyncConfirmationMultiplier", c.StateSyncConfirmationMultiplier)
 	return c.fetchStateSyncMultiplyerHelper(c.StateSyncConfirmationMultiplier, number)
 }
 
