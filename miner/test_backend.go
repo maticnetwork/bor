8f77dad94db7ef754c31257b6a80cda520363348
Shivam Sharma <shivam691999@gmail.com>
2023-05-05 18:03:27 +0530

    fix : fix lint

--- miner/test_backend.go
+++ miner/test_backend.go
@@ -358,6 +358,7 @@ func (w *worker) mainLoopWithDelay(ctx context.Context, delay uint) {
 				txset := types.NewTransactionsByPriceAndNonce(w.current.signer, txs, cmath.FromBig(w.current.header.BaseFee))
 				tcount := w.current.tcount
 
+				//nolint:contextcheck
 				w.commitTransactions(w.current, txset, nil, context.Background())
 
 				// Only update the snapshot if any new transactions were added
@@ -582,6 +583,7 @@ func (w *worker) commitWorkWithDelay(ctx context.Context, interrupt *int32, noem
 		return
 	}
 
+	//nolint:contextcheck
 	var interruptCtx = context.Background()
 
 	stopFn := func() {}
