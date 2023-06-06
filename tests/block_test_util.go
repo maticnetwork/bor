17fa1d6ce031856ebd3ffaede526df928ce43e14
Martin Holst Swende <martin@swende.se>
2023-06-06 04:27:40 -0400

    cmd/evm: make evm blocktest output logs if so instructed (#27396)
    * cmd/evm: make evm blocktest output logs if so instructed
    
    * Apply suggestions from code review
    
    Co-authored-by: Marius van der Wijden <m.vanderwijden@live.de>
    
    ---------
    
    Co-authored-by: Marius van der Wijden <m.vanderwijden@live.de>

--- tests/block_test_util.go
+++ tests/block_test_util.go
@@ -100,7 +100,7 @@ type btHeaderMarshaling struct {
 	BaseFeePerGas *math.HexOrDecimal256
 }
 
-func (t *BlockTest) Run(snapshotter bool) error {
+func (t *BlockTest) Run(snapshotter bool, tracer vm.EVMLogger) error {
 	config, ok := Forks[t.json.Network]
 	if !ok {
 		return UnsupportedForkError{t.json.Network}
@@ -126,8 +126,9 @@ func (t *BlockTest) Run(snapshotter bool) error {
 		cache.SnapshotLimit = 1
 		cache.SnapshotWait = true
 	}
-
-	chain, err := core.NewBlockChain(db, cache, gspec, nil, engine, vm.Config{}, nil, nil, nil)
+	chain, err := core.NewBlockChain(db, cache, gspec, nil, engine, vm.Config{
+		Tracer: tracer,
+	}, nil, nil)
 	if err != nil {
 		return err
 	}
