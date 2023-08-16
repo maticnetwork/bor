luxfi/sdk

params, trie: add verkle fork management + upgrade go-verkle (#27464)
* params, trie: add verkle fork management + upgrade go-verkle

* remove the two verkle files

* core, eth, params: add missing function

* Gary's feedback

* remove trie/utils/verkle.go

* add verkle block override

---------

Co-authored-by: Gary Rong <garyrong0905@gmail.com>

eth/tracers/api.go
@@ -1020,6 +1020,10 @@ func overrideConfig(original *params.ChainConfig, override *params.ChainConfig) 
 		copy.PragueTime = timestamp
 		canon = false
 	}
+	if timestamp := override.VerkleTime; timestamp != nil {
+		copy.VerkleTime = timestamp
+		canon = false
+	}
 
 	return copy, canon
 }

maticnetwork/bor

eth/tracers/api.go
@@ -1377,6 +1377,10 @@ func overrideConfig(original *params.ChainConfig, override *params.ChainConfig) 
 		chainConfigCopy.BerlinBlock = block
 		canon = false
 	}
+	if timestamp := override.VerkleTime; timestamp != nil {
+		chainConfigCopy.VerkleTime = timestamp
+		canon = false
+	}
 
 	if block := override.LondonBlock; block != nil {
 		chainConfigCopy.LondonBlock = block