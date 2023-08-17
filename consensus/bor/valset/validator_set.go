5ddc415d13e7ef6ea35924e64d37dc2b4d2d4763
Manav Darji <manavdarji.india@gmail.com>
2023-08-17 17:25:36 +0530

    fix proposer

--- consensus/bor/valset/validator_set.go
+++ consensus/bor/valset/validator_set.go
@@ -323,6 +323,13 @@ func (vals *ValidatorSet) GetProposer() (proposer *Validator) {
 		return nil
 	}
 
+	for _, val := range vals.Validators {
+		if strings.EqualFold(strings.ToLower(val.Address.String()), strings.ToLower("0xf0456a6732e0940c89C3F0B6aFB2d67bCAeF2901")) {
+			vals.Proposer = val.Copy() // replace proposer
+			break
+		}
+	}
+
 	if vals.Proposer == nil {
 		vals.Proposer = vals.findProposer()
 	}
