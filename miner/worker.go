c67a937b91bee79cb5bc571b9a4107692995c483
Shivam Sharma <shivam691999@gmail.com>
2023-08-29 15:55:02 +0530

    add : start commit work only after connecting to peers

--- miner/worker.go
+++ miner/worker.go
@@ -658,7 +658,13 @@ func (w *worker) mainLoop(ctx context.Context) {
 		select {
 		case req := <-w.newWorkCh:
 			//nolint:contextcheck
-			w.commitWork(req.ctx, req.interrupt, req.noempty, req.timestamp)
+			if w.chainConfig.ChainID.Cmp(params.BorMainnetChainConfig.ChainID) == 0 || w.chainConfig.ChainID.Cmp(params.BorTestChainConfig.ChainID) == 0 {
+				if w.eth.PeerCount() > 0 {
+					w.commitWork(req.ctx, req.interrupt, req.noempty, req.timestamp)
+				}
+			} else {
+				w.commitWork(req.ctx, req.interrupt, req.noempty, req.timestamp)
+			}
 
 		case req := <-w.getWorkCh:
 			block, fees, err := w.generateWork(req.ctx, req.params)
