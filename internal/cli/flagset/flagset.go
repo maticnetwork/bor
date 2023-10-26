1a45b4eccaee511e29e49ddb9cf2e93571fc3477
leovct <l009.vincent@gmail.com>
2023-10-26 21:44:02 +0200

    fix: lint issues

--- internal/cli/flagset/flagset.go
+++ internal/cli/flagset/flagset.go
@@ -55,10 +55,11 @@ func (f *Flagset) Help() string {
 
 	items := []string{}
 
-	flags := make([]*FlagVar, len(f.flags))
+	flags := []*FlagVar{}
 	for _, item := range f.flags {
 		flags = append(flags, item)
 	}
+	//nolint: wsl
 	sort.Sort(ByName(flags))
 
 	for _, item := range flags {
