4f5d43fa377a948435f63b7871ff81ed5033a30d
kislogic <nick.kisluhin@gmail.com>
2023-11-08 12:01:44 +0200

    enable logs

--- eth/tracers/native/call.go
+++ eth/tracers/native/call.go
@@ -253,7 +253,7 @@ func (t *callTracer) CaptureTxEnd(restGas uint64) {
 	t.callstack[0].GasUsed = t.gasLimit - restGas
 	if t.config.WithLog {
 		// Logs are not emitted when the call fails
-		clearFailedLogs(&t.callstack[0], false)
+		//clearFailedLogs(&t.callstack[0], false)
 	}
 }
 
