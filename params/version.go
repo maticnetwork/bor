5f435099b355dd252edc80e3e21261dde17a5367
Arpit Temani <temaniarpit27@gmail.com>
2024-02-29 08:45:43 +0530

    update version

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	VersionMajor = 1       // Major version component of the current release
 	VersionMinor = 2       // Minor version component of the current release
 	VersionPatch = 7       // Patch version component of the current release
-	VersionMeta  = "beta3" // Version metadata to append to the version string
+	VersionMeta  = "beta4" // Version metadata to append to the version string
 )
 
 var GitCommit string
