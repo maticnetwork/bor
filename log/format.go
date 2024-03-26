855084049d6e81f8fd519db2426ec83c65114a65
dongjinlong <dongjinlong@outlook.com>
2024-03-26 23:13:58 +0800

    chore: fix some comments
    Signed-off-by: dongjinlong <dongjinlong@outlook.com>

--- log/format.go
+++ log/format.go
@@ -40,7 +40,7 @@ func PrintOrigins(print bool) {
 var locationEnabled atomic.Bool
 
 // locationLength is the maxmimum path length encountered, which all logs are
-// padded to to aid in alignment.
+// padded to aid in alignment.
 var locationLength atomic.Uint32
 
 // fieldPadding is a global map with maximum field value lengths seen until now
