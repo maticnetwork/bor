6d74e0b466cd239cfe000562644aaf4815f56ec2
rjl493456442 <garyrong0905@gmail.com>
2024-11-09 08:08:06 +0800

    core/state, triedb/database: refactor state reader (#30712)
    Co-authored-by: Martin HS <martin@swende.se>

--- triedb/database/database.go
+++ triedb/database/database.go
@@ -16,7 +16,10 @@
 
 package database
 
-import "github.com/ethereum/go-ethereum/common"
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+)
 
 // Reader wraps the Node method of a backing trie reader.
 type Reader interface {
@@ -34,4 +37,32 @@ type Database interface {
 	// Reader returns a node reader associated with the specific state.
 	// An error will be returned if the specified state is not available.
 	Reader(stateRoot common.Hash) (Reader, error)
+}
+
+// StateReader wraps the Account and Storage method of a backing state reader.
+type StateReader interface {
+	// Account directly retrieves the account associated with a particular hash in
+	// the slim data format. An error will be returned if the read operation exits
+	// abnormally. Specifically, if the layer is already stale.
+	//
+	// Note:
+	// - the returned account object is safe to modify
+	// - no error will be returned if the requested account is not found in database
+	Account(hash common.Hash) (*types.SlimAccount, error)
+
+	// Storage directly retrieves the storage data associated with a particular hash,
+	// within a particular account. An error will be returned if the read operation
+	// exits abnormally.
+	//
+	// Note:
+	// - the returned storage data is not a copy, please don't modify it
+	// - no error will be returned if the requested slot is not found in database
+	Storage(accountHash, storageHash common.Hash) ([]byte, error)
+}
+
+// StateDatabase wraps the methods of a backing state store.
+type StateDatabase interface {
+	// StateReader returns a state reader associated with the specific state.
+	// An error will be returned if the specified state is not available.
+	StateReader(stateRoot common.Hash) (StateReader, error)
 }
