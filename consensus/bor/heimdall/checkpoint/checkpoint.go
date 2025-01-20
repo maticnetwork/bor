9295e5f73b7c2c1132642463adf803fb027caf0d
Angel Valkov <avalkov@polygon.technology>
2025-01-20 14:36:08 +0200

    Custom unmarshaling for checkpoint

--- consensus/bor/heimdall/checkpoint/checkpoint.go
+++ consensus/bor/heimdall/checkpoint/checkpoint.go
@@ -1,6 +1,11 @@
 package checkpoint
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"strconv"
+
 	"github.com/ethereum/go-ethereum/common"
 )
 
@@ -12,6 +17,49 @@ type Checkpoint struct {
 	RootHash   common.Hash    `json:"root_hash"`
 	BorChainID string         `json:"bor_chain_id"`
 	Timestamp  uint64         `json:"timestamp"`
+}
+
+func (m *Checkpoint) UnmarshalJSON(data []byte) error {
+	type Alias Checkpoint
+	temp := &struct {
+		StartBlock string `json:"start_block"`
+		EndBlock   string `json:"end_block"`
+		RootHash   string `json:"root_hash"`
+		Timestamp  string `json:"timestamp"`
+		*Alias
+	}{
+		Alias: (*Alias)(m),
+	}
+
+	if err := json.Unmarshal(data, temp); err != nil {
+		return err
+	}
+
+	startBlock, err := strconv.ParseUint(temp.StartBlock, 10, 64)
+	if err != nil {
+		return fmt.Errorf("invalid start_block: %w", err)
+	}
+	m.StartBlock = startBlock
+
+	endBlock, err := strconv.ParseUint(temp.EndBlock, 10, 64)
+	if err != nil {
+		return fmt.Errorf("invalid end_block: %w", err)
+	}
+	m.EndBlock = endBlock
+
+	decodedHash, err := base64.StdEncoding.DecodeString(temp.RootHash)
+	if err != nil {
+		return fmt.Errorf("failed to decode hash: %w", err)
+	}
+	m.RootHash = common.BytesToHash(decodedHash)
+
+	timestamp, err := strconv.ParseUint(temp.Timestamp, 10, 64)
+	if err != nil {
+		return fmt.Errorf("invalid timestamp: %w", err)
+	}
+	m.Timestamp = timestamp
+
+	return nil
 }
 
 type CheckpointResponse struct {
