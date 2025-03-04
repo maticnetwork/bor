fc2462d0ee52c0bbd1bd5a686c5c3873f24584c9
Manav Darji <manavdarji.india@gmail.com>
2025-03-04 19:39:43 +0530

    consensus/bor: add contract equivalent logic to find span by block number

--- consensus/bor/span_store.go
+++ consensus/bor/span_store.go
@@ -2,6 +2,7 @@ package bor
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/ethereum/go-ethereum/consensus/bor/heimdall/span"
 	"github.com/ethereum/go-ethereum/log"
@@ -11,8 +12,9 @@ import (
 // SpanStore acts as a simple middleware to cache span data populated from heimdall. It is used
 // in multiple places of bor consensus for verification.
 type SpanStore struct {
-	store          *lru.ARCCache
-	heimdallClient IHeimdallClient
+	store             *lru.ARCCache
+	latestKnownSpanId uint64
+	heimdallClient    IHeimdallClient
 }
 
 func NewSpanStore(heimdallClient IHeimdallClient) SpanStore {
@@ -21,19 +23,6 @@ func NewSpanStore(heimdallClient IHeimdallClient) SpanStore {
 		store:          cache,
 		heimdallClient: heimdallClient,
 	}
-}
-
-const (
-	defaultSpanLength = 6400 // Default span length i.e. number of bor blocks in a span
-	zerothSpanEnd     = 255  // End block of 0th span
-)
-
-// SpanIdAt returns the corresponding span id for the given block number.
-func spanIdAt(blockNum uint64) uint64 {
-	if blockNum > zerothSpanEnd {
-		return 1 + (blockNum-zerothSpanEnd-1)/defaultSpanLength
-	}
-	return 0
 }
 
 // spanById returns a span given its id. It fetches span from heimdall if not found in cache.
@@ -51,13 +40,36 @@ func (s *SpanStore) spanById(ctx context.Context, spanId uint64) (*span.Heimdall
 			return nil, err
 		}
 		s.store.Add(spanId, currentSpan)
+		if currentSpan.Span.ID > s.latestKnownSpanId {
+			s.latestKnownSpanId = currentSpan.ID
+		}
 	}
 
 	return currentSpan, nil
 }
 
-// spanByBlockNumber returns a span given a block number. It fetches span from heimdall if not found in cache.
+// spanByBlockNumber returns a span given a block number. It fetches span from heimdall if not found in cache. It
+// assumes that a span has been committed before (i.e. is current or past span) and returns an error if
+// asked for a future span. This is safe to assume as we don't have a way to find out span id for a future block
+// unless we hardcode the span length (which we don't want to).
 func (s *SpanStore) spanByBlockNumber(ctx context.Context, blockNumber uint64) (*span.HeimdallSpan, error) {
-	spanId := spanIdAt(blockNumber)
-	return s.spanById(ctx, spanId)
+	// Iterate over all spans and check for number. This is to replicate the behaviour implemented in
+	// https://github.com/maticnetwork/genesis-contracts/blob/master/contracts/BorValidatorSet.template#L118-L134
+	// This logic is independent of the span length (bit extra effort but maintains equivalence) and will work
+	// for all span lengths (even if we change it in future).
+	for id := s.latestKnownSpanId; id >= 0; id-- {
+		span, err := s.spanById(ctx, id)
+		if err != nil {
+			return nil, err
+		}
+		if blockNumber >= span.StartBlock && blockNumber <= span.EndBlock {
+			return span, nil
+		}
+		// Check if block number given is out of bounds
+		if id == s.latestKnownSpanId && blockNumber > span.EndBlock {
+			break
+		}
+	}
+
+	return nil, fmt.Errorf("span not found for block %d", blockNumber)
 }
