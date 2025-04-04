4157fe3eb491b260749c3cbf673c4213581dd21f
kamuikatsurgi <shahkrishang11@gmail.com>
2025-04-04 12:43:59 +0530

    fix: Bor-HV2 tests

--- consensus/bor/heimdall/checkpoint/checkpoint.go
+++ consensus/bor/heimdall/checkpoint/checkpoint.go
@@ -1,10 +1,7 @@
 package checkpoint
 
 import (
-	"encoding/base64"
 	"encoding/json"
-	"fmt"
-	"strconv"
 
 	"github.com/ethereum/go-ethereum/common"
 )
@@ -20,44 +17,27 @@ type Checkpoint struct {
 }
 
 func (m *Checkpoint) UnmarshalJSON(data []byte) error {
-	type Alias Checkpoint
-	temp := &struct {
-		StartBlock string `json:"start_block"`
-		EndBlock   string `json:"end_block"`
+	// Define a temp struct that matches the JSON structure.
+	var temp struct {
+		Proposer   string `json:"proposer"`
+		StartBlock uint64 `json:"start_block"`
+		EndBlock   uint64 `json:"end_block"`
 		RootHash   string `json:"root_hash"`
-		Timestamp  string `json:"timestamp"`
-		*Alias
-	}{
-		Alias: (*Alias)(m),
+		BorChainID string `json:"bor_chain_id"`
+		Timestamp  uint64 `json:"timestamp"`
 	}
 
-	if err := json.Unmarshal(data, temp); err != nil {
+	// Unmarshal the JSON into the temp struct.
+	if err := json.Unmarshal(data, &temp); err != nil {
 		return err
 	}
 
-	startBlock, err := strconv.ParseUint(temp.StartBlock, 10, 64)
-	if err != nil {
-		return fmt.Errorf("invalid start_block: %w", err)
-	}
-	m.StartBlock = startBlock
-
-	endBlock, err := strconv.ParseUint(temp.EndBlock, 10, 64)
-	if err != nil {
-		return fmt.Errorf("invalid end_block: %w", err)
-	}
-	m.EndBlock = endBlock
-
-	decodedHash, err := base64.StdEncoding.DecodeString(temp.RootHash)
-	if err != nil {
-		return fmt.Errorf("failed to decode hash: %w", err)
-	}
-	m.RootHash = common.BytesToHash(decodedHash)
-
-	timestamp, err := strconv.ParseUint(temp.Timestamp, 10, 64)
-	if err != nil {
-		return fmt.Errorf("invalid timestamp: %w", err)
-	}
-	m.Timestamp = timestamp
+	m.Proposer = common.HexToAddress(temp.Proposer)
+	m.StartBlock = temp.StartBlock
+	m.EndBlock = temp.EndBlock
+	m.RootHash = common.HexToHash(temp.RootHash)
+	m.BorChainID = temp.BorChainID
+	m.Timestamp = temp.Timestamp
 
 	return nil
 }
