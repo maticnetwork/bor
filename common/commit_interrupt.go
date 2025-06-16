ff5daad58138ae42270614122c60d83cd498fc45
Manav Darji <manavdarji.india@gmail.com>
2025-06-17 00:04:51 +0530

    remove global variable for commit interrupt

--- common/commit_interrupt.go
+++
@@ -1,9 +0,0 @@
-package common
-
-import "sync/atomic"
-
-// InterruptBlockBuilding is a global flag which will be set to true to notify
-// the miner to stop building the current block. It's also used in EVM interpreter
-// to interrupt an ongoing transaction execution. It's reset to false when block
-// building time of the next block starts.
-var InterruptBlockBuilding atomic.Bool
