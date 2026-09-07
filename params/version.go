4b07a05297142fe790087a9d43e4b79862ac538b
Pratik Patil <pratikspatil024@gmail.com>
2026-09-07 17:19:45 +0530

    params: version bump to v2.10.2-beta

--- params/version.go
+++ params/version.go
@@ -25,8 +25,8 @@ import (
 const (
 	VersionMajor = 2  // Major version component of the current release
 	VersionMinor = 10 // Minor version component of the current release
-	VersionPatch = 1  // Patch version component of the current release
-	VersionMeta  = "" // Version metadata to append to the version string
+	VersionPatch = 2  // Patch version component of the current release
+	VersionMeta  = "beta" // Version metadata to append to the version string
 )
 
 var (
